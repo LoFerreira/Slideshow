bd6e35004bb523d60f2a8b1545291acc730c9203
Leonardo Ferreira <leonardo10sp@gmail.com>
2021-06-16 16:54:22 -0300

    arrumando bug width

--- script.js
+++ script.js
@@ -6,10 +6,27 @@ document.querySelector('.slider--width').style.width =
 document.querySelector('.slider--controls').style.height = 
     `${document.querySelector('.slider').clientHeight}px`;
 
-    function goPrev() {
-
+function goPrev() {
+    currentSlide--;
+    if(currentSlide < 0) {
+        currentSlide = totalSlides - 1;
     }
+    updateMargin();
+}
 
-    function goNext() {
-
+function goNext() {
+    currentSlide++;
+    if(currentSlide > (totalSlides - 1)){
+        currentSlide = 0;
     }
+    updateMargin();
+}
+
+function updateMargin() {
+    let sliderItemWidth = document.querySelector('.slider--item').clientWidth;
+    let newMargin = (currentSlide * sliderItemWidth);
+    document.querySelector('.slider--width').style.marginLeft = 
+        `-${newMargin}px`
+}
+
+setInterval(goNext, 4000);
